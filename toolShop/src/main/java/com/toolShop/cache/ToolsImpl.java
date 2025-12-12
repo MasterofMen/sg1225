429d60b0f2f5d96918199a711842da658b329a2d
Sean Grano <grano2@live.com>
2025-12-12 13:50:42 -0500

    init

---
+++ toolShop/src/main/java/com/toolShop/cache/ToolsImpl.java
@@ -0,0 +1,19 @@
+package com.toolShop.cache;
+
+public interface ToolsImpl {
+    public String toolCode;
+    public String toolType;
+    public String brand;
+
+    public String getToolCode();
+
+    public void setToolCode(String toolCode);
+
+    public String getToolType();
+
+    public void setToolType(String toolType);
+
+    public String getBrand();
+
+    public void setBrand(String brand);
+}
